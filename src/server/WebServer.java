f6f1e82f81368f227eee08449d976eae46d948f6
Kyle <Kyle@192.168.1.36>
2014-08-25 13:18:18 -0400

    Added GetViewCount

--- src/server/WebServer.java
+++ src/server/WebServer.java
@@ -41,6 +41,7 @@ public class WebServer extends NanoHTTPD
 
     private static final String GET_IMAGE_ID_URI = "/GET_ID";
     private static final String GET_IMAGE_URI = "/GET_IMAGE";
+    private static final String GET_COUNT_URI = "/GET_COUNT";
     private static final String FAV_ICO = "/favicon.ico";
     
     private final String XSS_KEY = "Access-Control-Allow-Origin";
@@ -67,6 +68,7 @@ public class WebServer extends NanoHTTPD
             int newImageId = dw.addNewImage();
 
             json.put("id", newImageId);
+            
             return new NanoHTTPD.Response(json.toString());
         }
         //returns the generated image and notates a view of the image
@@ -96,6 +98,7 @@ public class WebServer extends NanoHTTPD
                     json.put("error", "Image not found");
                     response = new Response(json.toString());
                     response.addHeader(XSS_KEY, XSS_VALUE);
+                    response.setMimeType("application/json");
                     response.setStatus(Response.Status.INTERNAL_ERROR);
                     return response;
                 }
@@ -111,11 +114,35 @@ public class WebServer extends NanoHTTPD
             //error in parsing of number or getting file
             json.put("error", "Image could not be served");
             response = new Response(json.toString());
+            response.setMimeType("application/json");
             response.addHeader(XSS_KEY, XSS_VALUE);
             response.setStatus(Response.Status.INTERNAL_ERROR);
             return response;
         }
-
+        //gets the count for the number of views
+        else if (URI.startsWith(GET_IMAGE_URI + "/"))
+        {
+            Response response = new Response("");
+            JSONObject json = new JSONObject();
+            int requestID = Integer.parseInt(session.getUri().trim().replaceFirst(GET_IMAGE_URI + "/", ""));
+            int viewcount = -1;
+            if((viewcount = dw.getViewcount(requestID)) != -1)
+            {
+                json.put("viewcount", viewcount);
+                response = new Response(json.toString());
+                response.setStatus(Response.Status.OK);
+                response.setMimeType("application/json");
+                return response;
+            }
+            else
+            {
+                json.put("error", "Viewcount failed");
+                response = new Response(json.toString());
+                response.setMimeType("application/json");
+                response.setStatus(Response.Status.INTERNAL_ERROR);
+                return response;
+            }
+        }
         else if (URI.equals(FAV_ICO))
         {
             try
